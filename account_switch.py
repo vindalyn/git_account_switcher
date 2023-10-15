0a312d57ced1e646e3d82d80a23b259dca1ac94a
Vindalyn <vindalynne@outlook.com>
2023-10-15 17:21:16 +0200

    Changed account_switch.py to remove the github windows credentials! the login process after does have to be triggered manually, though n~n

--- account_switch.py
+++ account_switch.py
@@ -3,6 +3,47 @@ import json
 import sys
 import time
 import subprocess
+import pywintypes
+import win32cred
+
+
+def get_github_username():
+    try:
+        credentials = win32cred.CredEnumerate(None, 0)  # Fetch all credentials
+        github_credentials = []
+
+        for cred in credentials:
+            # Check if the credential is related to GitHub
+            if "github.com" in cred['TargetName'].lower():
+                github_credentials.append(cred)
+
+        if not github_credentials:
+            return None  # No GitHub credentials found
+
+        # Assuming the first GitHub credential is the one you want
+        username = github_credentials[0]['UserName']
+        return username
+    except pywintypes.error as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def remove_github_credential():
+    try:
+        credentials = win32cred.CredEnumerate(None, 0)  # Fetch all credentials
+
+        for cred in credentials:
+            # Check if the credential is related to GitHub
+            if "github.com" in cred['TargetName'].lower():
+                try:
+                    win32cred.CredDelete(cred['TargetName'], cred['Type'], 1)  # Use CRED_PERSIST_SESSION (1)
+                    print(f"GitHub credential for {cred['TargetName']} removed.")
+                except pywintypes.error as e:
+                    print(f"Error removing GitHub credential for {cred['TargetName']}: {e}")
+
+    except pywintypes.error as e:
+        print(f"An error occurred: {e}")
+
 
 def exit_program(seconds: int, error_id: int):
     print("------------------")
@@ -56,9 +97,17 @@ git_username = f"git config --global user.name '{account['username']}'"
 git_email = f"git config --global user.email {account['email']}"
 
 try:
-    result_username = subprocess.run(git_username, shell=True, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    result_email = subprocess.run(git_email, shell=True, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result_username = subprocess.run(git_username, shell=True, check=True, text=True, stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE)
+    result_email = subprocess.run(git_email, shell=True, check=True, text=True, stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
     print(f"Changed git global config account to {account['username']}!!")
+    if get_github_username().lower() != account['username'].lower():
+        remove_github_credential()
+    else:
+        print("git config username is the same!")
+
+
 except subprocess.CalledProcessError as e:
     print("Error executing Git command:", e)
 
